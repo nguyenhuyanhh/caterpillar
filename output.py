df86b4c875143b1fa52cecf3d92aeeb4d36b2db3
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2017-04-01 14:58:44 +0800

    merged_train_tube

--- output.py
+++ output.py
@@ -1,3 +1,9 @@
+"""
+Caterpillar Tube Pricing
+
+Nguyen Huy Anh, Lee Vicson, Deon Seng, Oh Yoke Chew
+"""
+
 import os
 import numpy as np
 
@@ -11,6 +17,7 @@ TEST_FILE = os.path.join(DATA_DIR, 'test_set.csv')
 OUT_FILE = os.path.join(CUR_DIR, 'out.csv')
 EXTRACT_TRAIN_FILE = os.path.join(CUR_DIR, 'extracted_train.csv')
 EXTRACT_TUBE_FILE = os.path.join(CUR_DIR, 'extracted_tube.csv')
+MERGE_TRAIN_TUBE_FILE = os.path.join(CUR_DIR, 'merged_train_tube.csv')
 
 # put random price
 # with open(OUT_FILE, 'w') as out_:
@@ -20,6 +27,7 @@ EXTRACT_TUBE_FILE = os.path.join(CUR_DIR, 'extracted_tube.csv')
 
 
 def extract_train():
+    """Extract train_set.csv."""
     with open(TRAIN_FILE, 'r') as in_, open(EXTRACT_TRAIN_FILE, 'w') as out_:
         tmp = in_.readlines()
         head = tmp[0].strip().split(',')
@@ -31,6 +39,7 @@ def extract_train():
 
 
 def extract_tubes():
+    """Extract tube.csv."""
     with open(TUBE_FILE, 'r') as in_, open(EXTRACT_TUBE_FILE, 'w') as out_:
         tmp = in_.readlines()
         head_tmp = tmp[0].strip().split(',')
@@ -43,6 +52,33 @@ def extract_tubes():
                 out_.write(','.join(values_tmp) + '\n')
 
 
+def merge_train_tubes():
+    """Merge two data sets together."""
+    with open(EXTRACT_TRAIN_FILE, 'r') as train_, open(EXTRACT_TUBE_FILE, 'r') as tube_, open(MERGE_TRAIN_TUBE_FILE, 'w') as out_:
+        tmp_train = train_.readlines()
+        tmp_tube = tube_.readlines()
+        head_tmp = tmp_tube[0].strip().split(
+            ',') + [tmp_train[0].strip().split(',')[1]]
+        out_.write(','.join(head_tmp) + '\n')
+        for tube_line in tmp_tube[1:]:
+            tube_tmp = tube_line.strip().split(',')
+            key = tube_tmp[0]
+            for train_line in tmp_train[1:]:
+                train_tmp = train_line.strip().split(',')
+                if train_tmp[0] == key:
+                    value_tmp = tube_tmp + [train_tmp[1]]
+                    out_.write(','.join(value_tmp) + '\n')
+                    break
+
+
+def get_coefficients():
+    """
+    Get the coefficients for linear regression of tube cost.
+    Model: cost = f(diameter, wall, length, num_bends, bend_radius)
+    """
+    with open(MERGE_TRAIN_TUBE_FILE, 'r') as merged_:
+        pass
+
+
 if __name__ == '__main__':
-    extract_train()
-    extract_tubes()
+    get_coefficients()
